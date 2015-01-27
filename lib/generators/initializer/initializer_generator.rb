147e6518f2b8a8da92b70d70d642100ba0685cef
Alexey <41exmackevich@gmail.com>
2015-01-27 22:42:23 +0300

    initializer generator

---
+++ lib/generators/initializer/initializer_generator.rb
@@ -0,0 +1,13 @@
+module Generators
+  module Initializer
+    class InitializerGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc "Creates a CryptApi initializer"
+   
+      def copy_initializer_file
+        copy_file "crypt_api_initializer.rb", "config/initializers/#{file_name}.rb"
+      end
+    end
+  end
+end
