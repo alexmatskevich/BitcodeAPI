9f5656762c640432c8a294edfd32446cf3035999
Alexey <41exmackevich@gmail.com>
2015-01-27 22:49:30 +0300

    Fixied errors in generator

--- lib/generators/initializer/initializer_generator.rb
+++ lib/generators/initializer/initializer_generator.rb
@@ -1,13 +1,9 @@
-module Generators
-  module Initializer
-    class InitializerGenerator < Rails::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
+class InitializerGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
 
-      desc "Creates a CryptApi initializer"
-   
-      def copy_initializer_file
-        copy_file "crypt_api_initializer.rb", "config/initializers/#{file_name}.rb"
-      end
-    end
+  def copy_initializer_file
+    copy_file "crypt_api_initializer.rb", "config/initializers/#{file_name}.rb"
   end
+
 end
+
